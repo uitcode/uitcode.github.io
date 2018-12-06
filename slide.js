2705a1c37cf4006a13fe85cf3d2bb380e638886e
uitcode <43876981+uitcode@users.noreply.github.com>
2018-12-06 11:11:28 +0700

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,33 @@
+// Scroll reveal calls
+window.sr = ScrollReveal();
+
+sr.reveal('.slide-icon-1', {
+  delay: 200,
+  scale: 0
+});
+sr.reveal('.slide-icon-2', {
+  delay: 400,
+  scale: 0
+});
+sr.reveal('.slide-icon-3', {
+  delay: 600,
+  scale: 0
+});
+sr.reveal('.slide-icon-4', {
+  delay: 800,
+  scale: 0
+});
+sr.reveal('.sr-button', {
+  delay: 200,
+  distance: '15px',
+  origin: 'bottom',
+  scale: 0.8
+});
+sr.reveal('.sr-contact-1', {
+  delay: 200,
+  scale: 0
+});
+sr.reveal('.sr-contact-2', {
+  delay: 400,
+  scale: 0
+});
